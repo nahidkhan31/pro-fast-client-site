b97d519081c3481e1de57401b17fcf92cf2cda2b
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-06-30 13:29:46 +0600

    added more code or modify previous code

--- src/pages/Brands/Brands.jsx
+++ src/pages/Brands/Brands.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Logo1 from "../../assets/brands/amazon.png";
 import Logo2 from "../../assets/brands/casio.png";
 import Logo3 from "../../assets/brands/moonstar.png";
-
 import Logo4 from "../../assets/brands/randstad.png";
 import Logo5 from "../../assets/brands/start-people 1.png";
 import Logo6 from "../../assets/brands/start.png";
@@ -14,16 +13,16 @@ const Brands = () => {
   return (
     <section className="py-10 bg-gray-100">
       <div className="max-w-7xl mx-auto px-4">
-        <h2 className="text-2xl font-bold text-center mb-6">
+        <h2 className="text-2xl font-bold text-center mb-12">
           Trusted by Leading Brands
         </h2>
         <Marquee pauseOnHover speed={50} gradient={false}>
           {logos.map((logo, index) => (
-            <div key={index} className="mx-8 flex items-center">
+            <div key={index} className="mx-24 flex items-center">
               <img
                 src={logo}
                 alt={`client Logo ${index + 1}`}
-                className="h-16"
+                className="h-6"
               />
             </div>
           ))}
